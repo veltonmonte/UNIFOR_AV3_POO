11b1d8438222a1ca10d1096cee672dc6a0308688
veltonmonte <montevelton@gmail.com>
2025-11-18 20:20:56 -0300

    Add files via upload

---
+++ AV3_POO/src/Participante.java
@@ -0,0 +1,84 @@
+import java.io.*;
+
+public class Participante {
+    private int idParticipante;
+    private String nomeParticipante;
+    private String loginParticipante;
+    private String emailParticipante;
+    private String senhaParticipante;
+    private String telefoneParticipante;
+
+    public Participante(int idParticipante,
+                        String nomeParticipante,
+                        String loginParticipante,
+                        String emailParticipante,
+                        String senhaParticipante,
+                        String telefoneParticipante)
+    {
+        this.idParticipante = idParticipante;
+        this.nomeParticipante = nomeParticipante;
+        this.loginParticipante = loginParticipante;
+        this.emailParticipante = emailParticipante;
+        this.senhaParticipante = senhaParticipante;
+        this.telefoneParticipante = telefoneParticipante;
+    }
+
+
+    public String getNomeParticipante() {
+        return nomeParticipante;
+    }
+
+    public void setNomeParticipante(String nomeParticipante) {
+        this.nomeParticipante = nomeParticipante;
+    }
+
+    public int idParticipante() {
+        return idParticipante;
+    }
+
+    public Participante setIdParticipante(int idParticipante) {
+        this.idParticipante = idParticipante;
+        return this;
+    }
+
+    public String loguinParticipante() {
+        return loginParticipante;
+    }
+
+    public void setLoginParticipante(String loginParticipante) {
+        this.loginParticipante = loginParticipante;
+    }
+
+    public String getEmailParticipante() {
+        return emailParticipante;
+    }
+
+    public void setEmailParticipante(String emailParticipante) {
+        this.emailParticipante = emailParticipante;
+    }
+
+    public String senhaParticipante() {
+        return senhaParticipante;
+    }
+
+    public void setSenhaParticipante(String senhaParticipante) {
+        this.senhaParticipante = senhaParticipante;
+    }
+
+    public String telefoneParticipante() {
+        return telefoneParticipante;
+    }
+
+    public void setTelefoneParticipante(String telefoneParticipante) {
+        this.telefoneParticipante = telefoneParticipante;
+    }
+
+    //public Participante loginParticipante() throws IOException {}
+
+    //public void registrarParticipante() {}
+
+    //public void listarParticipantes() {}
+
+
+
+}
---
+++ AV3_POO/src/itemLeilao.java
@@ -0,0 +1,69 @@
+public class itemLeilao {
+    private int idItem;
+    private Leilao leilao;
+    private String descricaoItem;
+    private Double lanceMinimo;
+    private boolean itemArrematado;
+    private Lance lanceArrematante;
+    
+    public itemLeilao(int idItem,
+                      Leilao leilao,
+                      String descricaoItem,
+                      Double lanceMinimo,
+                      Lance lanceArrematante)
+    {
+        this.idItem = idItem;
+        this.leilao = leilao;
+        this.descricaoItem = descricaoItem;
+        this.lanceMinimo = lanceMinimo;
+        this.lanceArrematante = lanceArrematante;
+    }
+
+    public int getIdItem() {
+        return idItem;
+    }
+    public void setIdItem(int idItem) {
+        this.idItem = idItem;
+    }
+
+    public Leilao getLeilao() {
+        return leilao;
+    }
+
+    public void setLeilao(Leilao leilao) {
+        this.leilao = leilao;
+    }
+
+    public String getDescricaoItem() {
+        return descricaoItem;
+    }
+
+    public void setDescricaoItem(String descricaoItem) {
+        this.descricaoItem = descricaoItem;
+    }
+
+    public Double getLanceMinimo() {
+        return lanceMinimo;
+    }
+
+    public void setLanceMinimo(Double lanceMinimo) {
+        this.lanceMinimo = lanceMinimo;
+    }
+
+    public boolean isItemArrematado() {
+        return itemArrematado;
+    }
+
+    public void setItemArrematado(boolean itemArrematado) {
+        this.itemArrematado = itemArrematado;
+    }
+
+    public Lance getLanceArrematante() {
+        return lanceArrematante;
+    }
+
+    public void setLanceArrematante(Lance lanceArrematante) {
+        this.lanceArrematante = lanceArrematante;
+    }
+    
+}
